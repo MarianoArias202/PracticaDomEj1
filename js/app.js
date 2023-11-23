c644ea1db32e86e7553caae3f9fd4cb5da5a525a
mariano202 <marianoaarias202@gmail.com>
2023-11-23 00:07:33 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+let numerAleatorio = 0;
+
+function crearnumeroaleatorio(){
+ 
+    numerAleatorio = Math.floor(Math.random() * 100) + 1;
+   
+}
+
+
+function adivinarnumero(){
+
+    let numeroIngresado = parseInt(document.getElementById('numeroIngresado').value);
+    console.log(numeroIngresado)
+    console.log( numerAleatorio)
+     if(numeroIngresado ===  numerAleatorio ){
+
+       const parrafoNuevo = document.createElement('p') // <p></p>
+       parrafoNuevo.innerHTML = `Felicidades Adivinaste el numero`;
+       parrafoNuevo.className = 'lead'
+       const seccionInformacionExtra = document.getElementsByClassName('my-5');
+      
+       seccionInformacionExtra[1].appendChild(parrafoNuevo) 
+
+     }else if(numeroIngresado >  numerAleatorio){
+        const parrafoNuevo = document.createElement('p') // <p></p>
+        parrafoNuevo.innerHTML = `El numero ingresado es mayor al numero magico`;
+        parrafoNuevo.className = 'lead'
+        const seccionInformacionExtra = document.getElementsByClassName('my-5');
+     
+        seccionInformacionExtra[1].appendChild(parrafoNuevo) 
+    
+     }else{
+        const parrafoNuevo = document.createElement('p') // <p></p>
+       parrafoNuevo.innerHTML = `El numero ingresado es menor al numero magico`;
+       parrafoNuevo.className = 'lead'
+       const seccionInformacionExtra = document.getElementsByClassName('my-5');
+   
+       seccionInformacionExtra[1].appendChild(parrafoNuevo) 
+      
+     }
+}
